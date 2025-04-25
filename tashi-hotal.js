9d1bdb548012450ff63410f7103b0278863fd971
slhendup <sonamlhendupjr@gmail.com>
2025-04-25 14:02:17 +0600

    newly added

--- tashi-hotal.js
+++ tashi-hotal.js
@@ -3,6 +3,46 @@ const http = require("node:http");
 //define the port number the server will listern on
 const PORT = 3000;
 
+const rooms = [101, 102, 103, 104, 105];
+
+let roomInfo = {
+  101: {
+    roomName: 101,
+    bed: true,
+    table: false,
+    tv: true,
+    toilet: true,
+  },
+  102: {
+    roomName: 102,
+    bed: true,
+    table: false,
+    tv: true,
+    toilet: true,
+  },
+  103: {
+    roomName: 103,
+    bed: true,
+    table: false,
+    tv: true,
+    toilet: true,
+  },
+  104: {
+    roomName: 104,
+    bed: true,
+    table: false,
+    tv: true,
+    toilet: true,
+  },
+  105: {
+    roomName: 105,
+    bed: true,
+    table: false,
+    tv: true,
+    toilet: true,
+  },
+};
+
 //create the server
 const server = http.createServer(function (req, res) {
   console.log(`URL::${req.method} ::${req.url}`);
@@ -20,52 +60,17 @@ const server = http.createServer(function (req, res) {
       }
       if (parts[1] === "rooms") {
         if (parts[2]) {
-          let roomInfo = {
-            101: {
-              roomName: 101,
-              bed: true,
-              table: false,
-              tv: true,
-              toilet: true,
-            },
-            102: {
-              roomName: 102,
-              bed: true,
-              table: false,
-              tv: true,
-              toilet: true,
-            },
-            103: {
-              roomName: 103,
-              bed: true,
-              table: false,
-              tv: true,
-              toilet: true,
-            },
-            104: {
-              roomName: 104,
-              bed: true,
-              table: false,
-              tv: true,
-              toilet: true,
-            },
-            105: {
-              roomName: 105,
-              bed: true,
-              table: false,
-              tv: true,
-              toilet: true,
-            },
-          };
-
           const info = roomInfo[parts[2]];
 
           res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(info ? info : `No room found with the provided ${parts[2]} number`));//hendaling the default rooms
+          res.end(
+            JSON.stringify(
+              info ? info : `No room found with the provided ${parts[2]} number`
+            )
+          ); //hendaling the default rooms
           return;
         }
         res.writeHead(200, { "Content-Type": "application/json" });
-        const rooms = [101, 102, 103, 104, 105];
         res.end(JSON.stringify(rooms));
       }
 
@@ -75,9 +80,35 @@ const server = http.createServer(function (req, res) {
       }
     }
   }
+
   if (req.method === "POST") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello Sonam you are trying to create something");
+    if (parts[1] === "rooms") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        //when we r done receving the chunk of data
+        const newRoom = JSON.parse(body);
+        const roomNumber = Number(newRoom.roomName);
+
+        if (rooms.includes(roomNumber)) {
+          res.writeHead(400, { "Content-Type": "application/json" });
+
+          res.end(
+            JSON.stringify({
+              message: `Room with ${roomNumber} already present`,
+            })
+          );
+          return;
+        }
+        rooms.push(roomNumber);
+        roomInfo[roomNumber] = newRoom;
+
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "created", room: newRoom }));
+      });
+    }
   }
 });
 
